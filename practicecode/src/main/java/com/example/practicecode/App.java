6c7447971e560619df911132afa390202d5f2a34
Braaavs <lr.cjbravo@mmdc.mcl.edu.ph>
2026-03-23 16:06:50 +0800

    Start of Program

---
+++ practicecode/src/main/java/com/example/practicecode/App.java
@@ -0,0 +1,21 @@
+package com.example.practicecode;
+
+
+import java.util.Scanner;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.TreeMap;
+import java.io.BufferedReader;
+import java.io.FileReader;
+
+/**
+ * Start from scratch
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
